3249e9310d688ad715c0cd4214fce0bfd62d5dd5
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-10-09 11:39:18 +0300

    Первый коммит

---
+++ src/main/java/com/github/stanislavbukaevsky/informationaboutpurchasedtickets/InformationAboutPurchasedTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.github.stanislavbukaevsky.informationaboutpurchasedtickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationAboutPurchasedTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationAboutPurchasedTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/stanislavbukaevsky/informationaboutpurchasedtickets/InformationAboutPurchasedTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stanislavbukaevsky.informationaboutpurchasedtickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationAboutPurchasedTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
